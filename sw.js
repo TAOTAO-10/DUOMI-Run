b1952adf3489a64e025b9f40ebf5cdcddad166b0
TAOTAO-10 <yingjietao28@gmail.com>
2026-07-22 04:11:05 +0800

    Update game loading for new sprites

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "domi-run-v15";
+const CACHE_NAME = "domi-run-v17";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -10,7 +10,9 @@ const APP_SHELL = [
   "./assets/icons/domi-icon-192.png",
   "./assets/icons/domi-icon-512.png",
   "./assets/sprites/domi-steve-sprites-v1.png",
-  "./assets/sprites/domi-run-cycle-v2.png"
+  "./assets/sprites/domi-run-cycle-v2.png",
+  "./assets/sprites/domi-duck-run-v1.png",
+  "./assets/sprites/domi-obstacles-v1.png"
 ];
 
 self.addEventListener("install", (event) => {
